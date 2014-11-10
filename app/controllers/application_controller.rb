f1704fdaa5cc9abc4dcd35dc92a5d2ba32d0175d
Yung Dai <yungchidai@gmail.com>
2014-11-10 13:41:02 -0500

    10. Adding logic to display 'Signed in as' or 'Login or Sign up' in the application view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  #Create a current_user helper method to show the log in status of the user. If the user is logged in we want to display "Signed in as [USERNAME]" or else display "Login or Sign up"
+  def current_user
+    @current_user ||= User.find(sesson[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
 end
