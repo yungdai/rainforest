aa4482dbdc47eccfa25cdaf9c12bff62c527d96a
Yung Dai <yungchidai@gmail.com>
2014-11-10 14:57:52 -0500

    12. Writing the review routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
 
+
   # makes sure that the root of the website is /products/
   root 'products#index'
-  resources :products
 
   # appropriate routes for new users.  Makes sure that you can only use the new and create commands for the resources
   resources :users, only: [:new, :create]
 
 
   # appropriate routes for sessions.  Makes sure that you can only use new, create, and destroy resources.
-
   resources :sessions, only: [:new, :create, :destroy]
+
+  # appropiate routes for reviews inside the products path.  Makes sure that you can only use the show, create, and destroy resources
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
