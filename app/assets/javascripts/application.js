05e9cafbbb500027894a2fab396866f02d62a55c
Yung Dai <yungchidai@gmail.com>
2014-11-26 13:41:44 -0500

    Revert "got the pagination and infinite scroll working!"
    This reverts commit 868351ca43f9143a4eb97db8c4a2c19d2d644c5b.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,14 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
+$(document).on('ready page:load', function() {
+    if ($('.pagination').length) {
+        $(window).scroll(function() {
+            var url = $('.pagination span.next').children().attr('href');
+            if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+                $('.pagination').text("Fetching more products...");
+                return $.getScript(url);
+            }
+        });
+    }
+});
