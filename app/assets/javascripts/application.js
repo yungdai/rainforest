3671d6ccc37d9e4a54b3ad052760b3a7861e79f4
Yung Dai <yungchidai@gmail.com>
2014-11-26 13:41:59 -0500

    Revert 868351c..05e9caf
    This rolls back to commit 868351ca43f9143a4eb97db8c4a2c19d2d644c5b.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,14 +16,3 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-$(document).on('ready page:load', function() {
-    if ($('.pagination').length) {
-        $(window).scroll(function() {
-            var url = $('.pagination span.next').children().attr('href');
-            if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-                $('.pagination').text("Fetching more products...");
-                return $.getScript(url);
-            }
-        });
-    }
-});
